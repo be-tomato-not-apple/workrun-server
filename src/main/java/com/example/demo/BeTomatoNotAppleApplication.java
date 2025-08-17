04da78d945c72ffe7e9ef8ab1e9972c146495ce4
eeeeeaaan <wjdwjddla73@naver.com>
2025-08-17 14:17:56 +0900

    project setting

---
+++ src/main/java/com/example/demo/BeTomatoNotAppleApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeTomatoNotAppleApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BeTomatoNotAppleApplication.class, args);
+	}
+
+}
